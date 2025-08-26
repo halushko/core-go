6b296617dfe43d1a006440333814c2ec874d5562
halushko <dgalun@gmail.com>
2025-08-26 18:34:56 +0300

    add sqlite

---
+++ sqlite/database.go
@@ -0,0 +1,44 @@
+package sqlite
+
+import (
+	external "database/sql"
+)
+
+const dbPath = "/data/sqlite"
+
+type dbImpl struct {
+	Sqlite *external.DB
+}
+
+type DBI interface {
+	Find(table Table, by Column, byValue any, whatColumns ...Column) ([]map[string]any, error)
+}
+
+type DBInfo struct {
+	Name    string
+	Project string
+	Tables  []Table
+}
+type Table struct {
+	Name    string
+	Columns []Column
+}
+
+type Column struct {
+	Name          string
+	Type          SQLiteType
+	IsPrimaryKey  bool
+	IsNullable    bool
+	IsUnique      bool
+	Default       string
+	Autoincrement bool
+}
+
+type SQLiteType string
+
+const (
+	SQLiteInteger SQLiteType = "INTEGER"
+	SQLiteReal    SQLiteType = "REAL"
+	SQLiteText    SQLiteType = "TEXT"
+	SQLiteBlob    SQLiteType = "BLOB"
+)
---
+++ sqlite/init.go
@@ -0,0 +1,65 @@
+package sqlite
+
+import (
+	external "database/sql"
+	"fmt"
+	"log"
+	"path/filepath"
+	"strings"
+)
+
+func Init(dbInfo DBInfo) (DBI, error) {
+	db, err := external.Open("sqlite", filepath.Join(dbPath, dbInfo.Project, dbInfo.Name+".db"))
+	if err != nil {
+		log.Fatalf("[InitDB] Помилка при підключенні до БД: %v", err)
+		return nil, err
+	}
+
+	for _, dbTable := range dbInfo.Tables {
+		var query strings.Builder
+		query.WriteString(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s", dbTable.Name))
+		for i, col := range dbTable.Columns {
+			var c strings.Builder
+			c.WriteString(fmt.Sprintf("\n\t%s %s", col.Name, col.Type))
+
+			if col.IsPrimaryKey {
+				c.WriteString(" PRIMARY KEY")
+			} else if col.IsNullable {
+				c.WriteString(" NOT NULL")
+			}
+
+			if col.IsUnique {
+				c.WriteString(" UNIQUE")
+			}
+
+			if col.Autoincrement {
+				c.WriteString(" AUTOINCREMENT")
+			} else if col.Default != "" {
+				c.WriteString(" DEFAULT " + col.Default)
+			}
+
+			if i < len(dbTable.Columns)-1 {
+				c.WriteString(",\n")
+			} else {
+				c.WriteString("\n)")
+			}
+			query.WriteString(c.String())
+		}
+		log.Printf("[DEBUG] %s\n", query.String())
+		err = createTable(db, query.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &dbImpl{Sqlite: db}, nil
+}
+
+func createTable(db *external.DB, query string) error {
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Printf("[ERROR] Помилка при створенні таблиці torrent_files: %v", err)
+		return err
+	}
+	log.Println("[INFO] Таблиця ініційована")
+	return nil
+}
